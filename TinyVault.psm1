b229bb11545e6ade428755887480d1f81931bd86
gianlucanadir <gianlucanadir@gmail.com>
2026-03-08 18:16:15 +0100

    feat: update vault.json folder

--- TinyVault.psm1
+++ TinyVault.psm1
@@ -1,6 +1,13 @@
-$script:VaultPath = "$env:USERPROFILE\vault.json"
-
 Get-ChildItem "$PSScriptRoot\Public\*.ps1" | ForEach-Object { . $_.FullName }
 Get-ChildItem "$PSScriptRoot\Private\*.ps1" | ForEach-Object { . $_.FullName }
 
+$script:VaultPath = "$env:USERPROFILE\.tinyvault\vault.json"
+$dir = Split-Path $script:VaultPath
+
+if (-not (Test-Path $dir)) {
+    Write-Verbose "Creating directory $dir..."
+    New-Item -ItemType Directory -Path $dir | Out-Null
+}
+
+Write-Verbose "Initializing TinyVault..."
 Initialize-TinyVaultSession
